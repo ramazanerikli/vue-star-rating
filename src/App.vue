b9f732173eb2e449796ce097fa053fd836b32170
ramazanerikli <ramazanerikli1@gmail.com>
2020-04-30 11:26:43 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Rating :grade="3" :maxStars="5" :hasCounter="true" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Rating from './components/Rating.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Rating
+  },
+  beforeCreate: function() {
+    document.body.className = 'home';
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import '/styles/global.scss';
 </style>
---
+++ src/components/Rating.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="rating">
+    <ul class="list">
+      <li @click="rate(star)" v-for="star in maxStars" :class="{ 'active': star <= stars }" :key="star.stars" class="star">
+        <i :class="star <= stars ? 'fas fa-star' : 'far fa-star'"></i>
+      </li>
+    </ul>
+    <div v-if="hasCounter" class="counter">
+      <span class="score-rating">{{ stars }}</span>
+      <span class="divider">/</span>
+      <span class="score-max">{{ maxStars }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Rating',
+  props: ['grade', 'maxStars', 'hasCounter'],
+  data() {
+    return {
+      stars: this.grade
+    }
+  },
+  methods: {
+    rate(star) {
+      if (typeof star === 'number' && star <= this.maxStars && star >= 0) {
+        this.stars = this.stars === star ? star - 1 : star
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import '../styles/rating.scss';
+</style>
