8a0b6e30785264fc98c47a9041a2d792fb7522ca
ramazanerikli <ramazanerikli1@gmail.com>
2020-04-30 16:15:59 +0300

    docs folder

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ramazanerikli/vue-star-rating.git master:vue-star-rating
+git push -f git@github.com:ramazanerikli/vue-star-rating.git master:gh-pages
 
 cd -
